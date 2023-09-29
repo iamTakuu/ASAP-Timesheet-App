d514d3d7c273533633a4fff7055e8ecf6323d7ea
Ryan-Mythic <91421957+Ryan-Mythic@users.noreply.github.com>
2023-09-29 11:39:40 +0200

    Added Start & end date button back
    added start/end date selectors back to stats screen.

--- app/src/main/java/com/iamtakuu/asap_timesheet_app/screens/StatisticsScreen.kt
+++ app/src/main/java/com/iamtakuu/asap_timesheet_app/screens/StatisticsScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.iamtakuu.asap_timesheet_app.R
+import com.iamtakuu.asap_timesheet_app.components.DateTimePickerComponent
 import com.iamtakuu.asap_timesheet_app.components.HeadingTextComponent
 import com.iamtakuu.asap_timesheet_app.components.NormalTextComponent
 import com.iamtakuu.asap_timesheet_app.data.statistics.PieChart
@@ -39,6 +40,16 @@ private fun StatisticsShow(modifier: Modifier = Modifier) {
                 ) {
                     Column {
                         HeadingTextComponent(value = stringResource(id = R.string.statistics))
+
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        // Start Date
+                        DateTimePickerComponent(labelValue = stringResource(id = R.string.start_date))
+
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        // End Date
+                        DateTimePickerComponent(labelValue = stringResource(id = R.string.end_date))
 
                         Spacer(modifier = Modifier.height(50.dp))
 
